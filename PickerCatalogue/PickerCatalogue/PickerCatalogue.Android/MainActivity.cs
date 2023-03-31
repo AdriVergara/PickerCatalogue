fab9ddbc011e9703a7a6c52530649c4c962030d1
AdriVergara <adriveroca@gmail.com>
2023-03-31 11:51:42 +0200

    new app icon added on android

--- PickerCatalogue/PickerCatalogue/PickerCatalogue.Android/MainActivity.cs
+++ PickerCatalogue/PickerCatalogue/PickerCatalogue.Android/MainActivity.cs
@@ -12,7 +12,7 @@ using Acr.UserDialogs;
 
 namespace PickerCatalogue.Droid
 {
-    [Activity(Label = "PickerCatalogue", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "PickerCatalogue", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
